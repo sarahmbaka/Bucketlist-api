7c22e1a6c9b2111b9693852188101b7579e63ad0
sarah mbaka <salliedailey@gmail.com>
2017-07-25 18:27:30 +0300

    [FEATURE] add register endpoint

---
+++ app/endpoints.py
@@ -0,0 +1,12 @@
+"""End point definition."""
+from flask import Blueprint
+from flask_restful import Api
+
+# local imports
+from app.views import AuthRegister, AuthLogin, BucketlistView, ItemView
+# initialize api blueprint
+api_blueprint = Blueprint('api', __name__)
+# # initialize the api class
+api = Api(api_blueprint)
+
+api.add_resource(AuthRegister, '/auth/register')
