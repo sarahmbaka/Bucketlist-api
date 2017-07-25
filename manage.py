5f69d1dfe444cfb386b7de3f138dbc79c62fce37
sarah mbaka <salliedailey@gmail.com>
2017-07-25 17:55:49 +0300

    [FEATURE] add manage.py for migrations

---
+++ manage.py
@@ -0,0 +1,23 @@
+"""manage.py."""
+
+import os
+import unittest
+from flask_script import Manager  # class for handling a set of commands
+from flask_migrate import Migrate, MigrateCommand
+from app import db, create_app
+from app.models import User, Bucketlist, Item
+
+app = create_app(config_name='development')
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+# define our command for testing called "test"
+# Usage: python manage.py test
+
+
+
+if __name__ == '__main__':
+    manager.run()
