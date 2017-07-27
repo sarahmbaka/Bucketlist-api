9708553efb56f1a9cd3c44330181722c0a9c06bc
sarah mbaka <salliedailey@gmail.com>
2017-07-27 14:43:44 +0300

    [TEST] Delete bucketlist with unauthorized token

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -298,6 +298,20 @@ class TestBucketlist(unittest.TestCase):
         self.assertEqual("Bucketlist does not exist.!!",
                          res_message['message'])
 
+    def test_delete_bucketlist_with_unauthorized_token(self):
+        """Test that a user can update bucketlist."""
+        res_bucketlist1 = self.client.post('/bucketlist/',data= json.dumps(self.bucketlist),
+                                          headers=self.headers
+                                          ,content_type="application/json")
+        res_bucketlist = self.client.delete('/bucketlist/1',
+                                          headers=self.headers2
+                                          ,content_type="application/json")
+
+        res_message = json.loads(res_bucketlist.data.decode('utf8'))
+        self.assertEqual(res_bucketlist.status_code, 404)
+        self.assertEqual("Bucketlist does not exist.!!",
+                         res_message['message'])
+
     
 
 
